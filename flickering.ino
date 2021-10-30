182b463d7682d49c75e9e98b1077bbdc9230ab25
LakesideMiners <23389169+LakesideMiners@users.noreply.github.com>
2021-10-30 15:06:57 -0400

    new code

--- flickering.ino
+++ flickering.ino
@@ -1,215 +1,165 @@
-/* Oringal: https://github.com/schnoggo/jack-o-candle/blob/master/jack-o-candle.ino
-Modifed By: https://github.com/LakesideMiners/
-*/
-
-/*
- 5 "flames" of 3 pixels each.
- Each flame can have a brightness of 0 to 254 (play with this scale)
- Eventually, light up center pixel of three first then the sides. This version will just distribute amongst the 3 pixels.
-*/
 #include <Adafruit_NeoPixel.h>
-#define PIN 3              // Which pin are those Neopixels hook up to?
-#define NUMBER_OF_FLAMES 4 // depends on number of neopixel triplets. 5 for 16 NeoPixel ring. 4 for 12 NeoPixel ring
-#define FLAME_WIDTH 2      // How wide are the flames (in LEDs)
-#define FLICKER_CHANCE 3   // increase this to increase the chances an individual flame will flicker
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBER_OF_FLAMES * FLAME_WIDTH, PIN, NEO_GRB + NEO_KHZ800);
+// The onboard red LED's pin
+#define REDLED_PIN              13
+// The data-in pin of the NeoPixel
+#define WICK_PIN                3
+// Any unconnected pin, to try to generate a random seed
+#define UNCONNECTED_PIN         8
 
-uint32_t rez_range = 256 * 3;
-#define D_ false
+// The LED can be in only one of these states at any given time
+#define BRIGHT                  0
+#define UP                      1
+#define DOWN                    2
+#define DIM                     3
 
-// console buttons:
-struct flame_element
-{
-  int brightness;
-  int step;
-  int max_brightness;
-  long rgb[3];
-  byte state;
-} flames[NUMBER_OF_FLAMES];
+#define BRIGHT_HOLD             4
+#define DIM_HOLD                5
 
-int new_brightness = 0;
-unsigned long rgb[3]; //reusable temporary array
-uint8_t scaleD_rgb[3];
-byte acc;
+// Percent chance the LED will suddenly fall to minimum brightness
+#define INDEX_BOTTOM_PERCENT    10
+// Absolute minimum red value (green value is a function of red's value)
+#define INDEX_BOTTOM            128
+// Minimum red value during "normal" flickering (not a dramatic change)
+#define INDEX_MIN               192
+// Maximum red value
+#define INDEX_MAX               255
 
-#define SCALERVAL 256 * 3
-const int flamecolors[22][3] = {
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {SCALERVAL, 0, 0},
-    {
-        SCALERVAL,
-        SCALERVAL * .4,
-    },
-    {SCALERVAL, SCALERVAL * .4, 0},
-    {SCALERVAL, SCALERVAL * .4, 0},
-    {SCALERVAL, SCALERVAL * .4, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {SCALERVAL, SCALERVAL * .3, 0},
-    {
-        SCALERVAL,
-        SCALERVAL * .3,
-    },
-    {SCALERVAL, SCALERVAL * .3, SCALERVAL}, // white
-    {0, SCALERVAL * .2, SCALERVAL},         // that one blue flame
-    {SCALERVAL, SCALERVAL * .3, SCALERVAL * .5}};
+// Decreasing brightness will take place over a number of milliseconds in this range
+#define DOWN_MIN_MSECS          20
+#define DOWN_MAX_MSECS          250
+// Increasing brightness will take place over a number of milliseconds in this range
+#define UP_MIN_MSECS            20
+#define UP_MAX_MSECS            250
+// Percent chance the color will hold unchanged after brightening
+#define BRIGHT_HOLD_PERCENT     20
+// When holding after brightening, hold for a number of milliseconds in this range
+#define BRIGHT_HOLD_MIN_MSECS   0
+#define BRIGHT_HOLD_MAX_MSECS   100
+// Percent chance the color will hold unchanged after dimming
+#define DIM_HOLD_PERCENT        5
+// When holding after dimming, hold for a number of milliseconds in this range
+#define DIM_HOLD_MIN_MSECS      0
+#define DIM_HOLD_MAX_MSECS      50
+
+#define MINVAL(A,B)             (((A) < (B)) ? (A) : (B))
+#define MAXVAL(A,B)             (((A) > (B)) ? (A) : (B))
+
+Adafruit_NeoPixel *wick;
+byte state;
+unsigned long flicker_msecs;
+unsigned long flicker_start;
+byte index_start;
+byte index_end;
+
+void set_color(byte index)
+  {
+  index = MAXVAL(MINVAL(index, INDEX_MAX), INDEX_BOTTOM);
+  if (index >= INDEX_MIN)
+    wick->setPixelColor(0, index, (index * 3) / 8, 0);
+  else if (index < INDEX_MIN)
+    wick->setPixelColor(0, index, (index * 3.25) / 8, 0);
+
+  wick->show();
+  return;
+  }
 
 void setup()
-{
-  // if (D_){
-  Serial.begin(9600);
-  Serial.println("STARTUP");
-  //  }
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  randomSeed(analogRead(4));
-  InitFlames();
-}
+  {
+  // There is no good source of entropy to seed the random number generator,
+  // so we'll just read the analog value of an unconnected pin.  This won't be
+  // very random either, but there's really nothing else we can do.
+  //
+  // True randomness isn't strictly necessary, we just don't want a whole
+  // string of these things to do exactly the same thing at the same time if
+  // they're all powered on simultaneously.
+  randomSeed(analogRead(UNCONNECTED_PIN));
+
+  // Turn off the onboard red LED
+  pinMode(REDLED_PIN, OUTPUT);
+  digitalWrite(REDLED_PIN, LOW);
+
+  wick = new Adafruit_NeoPixel(1, WICK_PIN, NEO_RGB + NEO_KHZ800);
+  // wick = new Adafruit_NeoPixel(1, WICK_PIN); // for RGBW, if you see green uncomment this line
+
+  wick->begin();
+  wick->show();
+
+  set_color(255);
+  index_start = 255;
+  index_end = 255;
+  state = BRIGHT;
+
+  return;
+  }
 
 void loop()
-{
-
-  for (byte flame_count = 0; flame_count < NUMBER_OF_FLAMES; flame_count++)
   {
-    switch (flames[flame_count].state)
+  unsigned long current_time;
+
+  current_time = millis();
+
+  switch (state)
     {
-    case 0: // reset
-      CreateNewFlame(flame_count);
-      break;
-
-    case 1: //increasing
-      new_brightness = flames[flame_count].brightness + flames[flame_count].step;
-      if (new_brightness > flames[flame_count].max_brightness)
-      {
-        UpdateFlameColor(flame_count, flames[flame_count].max_brightness);
-        flames[flame_count].brightness = flames[flame_count].max_brightness;
-        flames[flame_count].step = GetStepSize(); // pick a different speed for flame going out
-        flames[flame_count].state = 2;
-      }
+    case BRIGHT:
+      flicker_msecs = random(DOWN_MAX_MSECS - DOWN_MIN_MSECS) + DOWN_MIN_MSECS;
+      flicker_start = current_time;
+      index_start = index_end;
+      if ((index_start > INDEX_BOTTOM) &&
+          (random(100) < INDEX_BOTTOM_PERCENT))
+        index_end = random(index_start - INDEX_BOTTOM) + INDEX_BOTTOM;
       else
-      {
-        UpdateFlameColor(flame_count, new_brightness);
-        flames[flame_count].brightness = new_brightness;
-      }
+        index_end = random(index_start - INDEX_MIN) + INDEX_MIN;
+
+      state = DOWN;
+      break;
+    case DIM:
+      flicker_msecs = random(UP_MAX_MSECS - UP_MIN_MSECS) + UP_MIN_MSECS;
+      flicker_start = current_time;
+      index_start = index_end;
+      index_end = random(INDEX_MAX - index_start) + INDEX_MIN;
+      state = UP;
+      break;
+    case BRIGHT_HOLD:
+    case DIM_HOLD:
+      if (current_time >= (flicker_start + flicker_msecs))
+        state = (state == BRIGHT_HOLD) ? BRIGHT : DIM;
 
       break;
-
-    case 2: //decreasing
-      new_brightness = flames[flame_count].brightness - flames[flame_count].step;
-      // chance to flicker/rekindle:
-      if (random(new_brightness) < FLICKER_CHANCE)
-      {
-        // rekindle:
-        flames[flame_count].state = 1; //increase again
-        flames[flame_count].brightness = max(GetMaxBrightness(), flames[flame_count].brightness);
-        flames[flame_count].step = GetStepSize();
-      }
+    case UP:
+    case DOWN:
+      if (current_time < (flicker_start + flicker_msecs))
+        set_color(index_start + ((index_end - index_start) * (((current_time - flicker_start) * 1.0) / flicker_msecs)));
       else
-      {
-        if (new_brightness < 1)
         {
-          flames[flame_count].state = 0; // bottomed out - reset to next flame
-          flames[flame_count].brightness = 0;
-          UpdateFlameColor(flame_count, 0);
-        }
+        set_color(index_end);
+
+        if (state == DOWN)
+          {
+          if (random(100) < DIM_HOLD_PERCENT)
+            {
+            flicker_start = current_time;
+            flicker_msecs = random(DIM_HOLD_MAX_MSECS - DIM_HOLD_MIN_MSECS) + DIM_HOLD_MIN_MSECS;
+            state = DIM_HOLD;
+            }
+          else
+            state = DIM;
+          }
         else
-        {
-          UpdateFlameColor(flame_count, new_brightness);
-          flames[flame_count].brightness = new_brightness;
+          {
+          if (random(100) < BRIGHT_HOLD_PERCENT)
+            {
+            flicker_start = current_time;
+            flicker_msecs = random(BRIGHT_HOLD_MAX_MSECS - BRIGHT_HOLD_MIN_MSECS) + BRIGHT_HOLD_MIN_MSECS;
+            state = BRIGHT_HOLD;
+            }
+          else
+            state = BRIGHT;
+          }
         }
-      }
+
       break;
     }
+
+  return;
   }
-  strip.show();
-  delay(22);
-
-} //loop()
-
-void InitFlames()
-{
-  // Sets initial states in flames array
-  for (byte i = 0; i < NUMBER_OF_FLAMES; i++)
-  {
-    flames[i].state = 0;
-  }
-}
-
-void UpdateFlameColor(byte flame_num, int new_brightness)
-{
-  //
-  uint32_t c = 0;
-  uint32_t color_channel_value;
-  byte rgb_channel;
-
-  new_brightness = min(new_brightness, flames[flame_num].max_brightness);
-
-  for (byte rgb_channel = 0; rgb_channel < 3; rgb_channel++)
-  {
-    color_channel_value = flames[flame_num].rgb[rgb_channel];
-    color_channel_value = color_channel_value * (uint32_t)new_brightness; // keep it long
-    color_channel_value = color_channel_value / (uint32_t)rez_range;
-    rgb[rgb_channel] = max(0L, color_channel_value);
-  } // step through R G B
-
-  // spread possible values of 0 -768 across 3 pixels
-  for (byte sub_pixel = 0; sub_pixel < FLAME_WIDTH; sub_pixel++)
-  {
-    for (byte i = 0; i < 3; i++)
-    { // rgb
-      acc = rgb[i] / 3;
-      byte d = rgb[i] % 3;
-      if (sub_pixel < d)
-      {
-        acc++;
-      }
-      scaleD_rgb[i] = acc;
-    }
-    c = strip.Color(scaleD_rgb[0], scaleD_rgb[1], scaleD_rgb[2]);
-    strip.setPixelColor(flame_num * FLAME_WIDTH + sub_pixel, c);
-  }
-}
-
-void CreateNewFlame(byte flame_num)
-{
-  flames[flame_num].step = GetStepSize();
-  flames[flame_num].max_brightness = GetMaxBrightness();
-
-  flames[flame_num].brightness = 0;
-  flames[flame_num].state = 1;
-  byte color_index = random(22);
-  for (byte i = 0; i < 3; i++)
-  {
-    flames[flame_num].rgb[i] = flamecolors[color_index][i];
-  }
-}
-
-int GetStepSize()
-{
-  return random(70) + 1;
-}
-int GetMaxBrightness()
-{
-  int retVal;
-  //  retVal = random(rez_range/4) +  random(rez_range/4) + random(rez_range/4) + rez_range/4 +1; // bell curve
-  //  retVal = random(rez_range*3/4) +  rez_range/4; // flat distribution
-  retVal = random(rez_range / 2) + rez_range / 2; // brighter flat distribution
-  return retVal;
-}
