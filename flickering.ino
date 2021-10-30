5d5e17286311b8a1b617a9a3027201792b600226
LakesideMiners <23389169+LakesideMiners@users.noreply.github.com>
2021-10-30 18:07:42 -0400

    fucking finaly

--- flickering.ino
+++ flickering.ino
@@ -1,122 +1,70 @@
-// FastLed
+//***************************************************************
+// Breathing effect
+// Color shifts from hueA to hueB as it pulses.
+//
+// Set A and B to the same hue if you don't want the color to
+// change.  Saturation for the high and low can also be set.
+//
+// Marc Miller, 2015
+// Updated Aug 2020 - removed delay, added dim8_video
+// Oct 2021 Moded by github.com/LakesideMiners
+//***************************************************************
+
 #include "FastLED.h"
-
-#define DATA_PIN    6
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    240 // Number of leds you are using
+#define LED_TYPE    WS2811  //WS2811, WS2812, WS2812B
+#define COLOR_ORDER RGB
+#define DATA_PIN 5 
+#define NUM_LEDS 24
+#define BRIGHTNESS 10
 CRGB leds[NUM_LEDS];
-#define BRIGHTNESS          96
-unsigned long lastLEDUpdate = 0;
 
-void setup() 
-{
+static float pulseSpeed = 0.5;  // Larger value gives faster pulse.
 
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  // set master brightness control
+uint8_t hueA = 100;  // Start hue at valueMin.
+uint8_t satA = 255;  // Start saturation at valueMin.
+float valueMin = 120.0;  // Pulse minimum value (Should be less then valueMax).
+
+uint8_t hueB = 80;  // End hue at valueMax.
+uint8_t satB = 255;  // End saturation at valueMax.
+float valueMax = 255.0;  // Pulse maximum value (Should be larger then valueMin).
+
+uint8_t hue = hueA;  // Do Not Edit
+uint8_t sat = satA;  // Do Not Edit
+float val = valueMin;  // Do Not Edit
+uint8_t hueDelta = hueA - hueB;  // Do Not Edit
+static float delta = (valueMax - valueMin) / 2.35040238;  // Do Not Edit
+
+
+//---------------------------------------------------------------
+void setup(){
+  Serial.begin(115200);  // Allows serial monitor output (check baud rate)
+  delay(500);  // Startup delay
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+  FastLED.show();
+  Serial.println("Setup done.  \n");
 }
 
-//Credit to Tweaking4ALL for original blinky eye code: http://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects/#halloween_eyes
 
-void loop() {
-  // Fixed:
-  // HalloweenEyes(0xff, 0x00, 0x00, 1,4, true, 10, 80, 3000);
-  // or Random:
-  HalloweenEyes(0xff, 0xdf, 0x0d, //Primary Color 1 That Fades
-                0xff, 0x00, 0x00, //Primary Color 2 that Does Not fade
-                1, random(2,4), 
-                true, random(5,50), random(50,150), 
-                random(500, 3000));
-  HalloweenEyes(0xb7, 0x00, 0xFe, //Secondary Color 1 That Fades
-                0xff, 0x00, 0x00, //Secondary Color 2 That Does Not Fade
-                1, random(1,4), 
-                true, random(15,75), random(100,250), 
-                random(500, 3000));
-  unsigned long now = millis();
-  if ( now - lastLEDUpdate > 10000 ) {
-    setAll(0,0,0); // Set all black every 10 seconds
-  lastLEDUpdate = now;
+//---------------------------------------------------------------
+void loop(){
+  float dV = ((exp(sin(pulseSpeed * millis()/2000.0*PI)) -0.36787944) * delta);
+  val = valueMin + dV;
+  hue = map(val, valueMin, valueMax, hueA, hueB);  // Map hue based on current val
+  sat = map(val, valueMin, valueMax, satA, satB);  // Map sat based on current val
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue, sat, val);
+
+    // You can experiment with commenting out these dim8_video lines
+    // to get a different sort of look.
+    leds[i].r = dim8_video(leds[i].r);
+    leds[i].g = dim8_video(leds[i].g);
+    leds[i].b = dim8_video(leds[i].b);
   }
-  
-}
 
-void HalloweenEyes(byte red, byte green, byte blue,
-                   byte red2, byte green2, byte blue2,  
-                   int EyeWidth, int EyeSpace, 
-                   boolean Fade, int Steps, int FadeDelay,
-                   int EndPause){
-  randomSeed(analogRead(0));
-  
-  int i;
-  int StartPoint  = random( 0, NUM_LEDS - (2*EyeWidth) - EyeSpace );
-  int Start2ndEye = StartPoint + EyeWidth + EyeSpace;
-  int StartPoint2  = random( 0, NUM_LEDS - (2*EyeWidth) - EyeSpace );
-  int Start2ndEye2 = StartPoint2 + EyeWidth + EyeSpace;
-  
-  for(i = 0; i < EyeWidth; i++) {
-    setPixel(StartPoint + i, red, green, blue);
-    setPixel(Start2ndEye + i, red, green, blue);
-    setPixel(StartPoint2 + i, red2, green2, blue2);
-    setPixel(Start2ndEye2 + i, red2, green2, blue2);
-  }
-  
-  showStrip();
-  
-  if(Fade==true) {
-    float r, g, b;
-  
-    for(int j = Steps; j >= 0; j--) {
-      r = j*(red/Steps);
-      g = j*(green/Steps);
-      b = j*(blue/Steps);
-      
-      for(i = 0; i < EyeWidth; i++) {
-        setPixel(StartPoint + i, r, g, b);
-        setPixel(Start2ndEye + i, r, g, b);
-      }
-      
-      showStrip();
-      delay(FadeDelay);
-    }
-  }
-  
-  //setAll(0,0,0); // Set all black
-  
-  delay(EndPause);
-}
-
-
-void showStrip() {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   strip.show();
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H
-   // FastLED
-   FastLED.show();
- #endif
-}
-
-void setPixel(int Pixel, byte red, byte green, byte blue) {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H 
-   // FastLED
-   leds[Pixel].r = red;
-   leds[Pixel].g = green;
-   leds[Pixel].b = blue;
- #endif
-}
-
-void setAll(byte red, byte green, byte blue) {
-  for(int i = 0; i < NUM_LEDS; i++ ) {
-    setPixel(i, red, green, blue); 
-  }
-  showStrip();
-}
-
+  FastLED.show();
+   
+} // end_main_loop
 
